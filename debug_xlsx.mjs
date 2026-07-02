3fbdc9007499f8c2ca997c52e6a64bcc4f0c8c30
winaiNun <winaicomputer18@gmail.com>
2026-07-02 11:03:50 +0700

    chore: remove debug files and update gitignore

--- debug_xlsx.mjs
+++
@@ -1,9 +0,0 @@
-import XLSX from 'xlsx'
-import { readFileSync } from 'node:fs'
-import { resolve } from 'node:path'
-
-const filePath = resolve(process.cwd(), 'public', '001.xlsx')
-console.log('file', filePath)
-const data = readFileSync(filePath)
-const wb = XLSX.read(data, { type: 'buffer' })
-console.log('sheetNames', wb.SheetNames)
